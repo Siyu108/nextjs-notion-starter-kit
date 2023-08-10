e27711000221ee6c92fecb88de1481d6313acc68
Siyu108 <107836406+Siyu108@users.noreply.github.com>
2023-08-10 14:51:39 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,13 +7,7 @@ module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
   images: {
     domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
+      'HSYHSY'
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
